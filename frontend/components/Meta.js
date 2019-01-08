03ece2c8a37b8f573d4f7093d0ac9a9a39a79169
Carlota Fernández <cavefe@gmail.com>
2019-01-08 11:17:40 +0100

    extract component meta

---
+++ frontend/components/Meta.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const Meta = _props => (
+  <Head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link ref="shortcut icon" href="static/favicon.png" type="image/x-icon" />
+    <link ref="stylesheat" href="static/nprogess.css" type="text/css" />
+    <title>Document</title>
+  </Head>
+);
+
+export default Meta;
--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import Meta from "./Meta";
 import Header from "./Header";
 
 class Page extends React.Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
         {this.props.children}
       </div>
