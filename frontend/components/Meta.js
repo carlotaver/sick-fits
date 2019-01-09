2f4a853bf7d8e310e84ebe1ad3993d60dc47c5d3
Carlota Fernández <cavefe@gmail.com>
2019-01-09 11:27:30 +0100

    changes in meta in frontend

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -3,9 +3,9 @@ import Head from "next/head";
 
 const Meta = _props => (
   <Head>
-    <meta charset="UTF-8" />
+    <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
     <link ref="shortcut icon" href="static/favicon.png" type="image/x-icon" />
     <link ref="stylesheat" href="static/nprogess.css" type="text/css" />
     <title>Sick Fits!</title>
