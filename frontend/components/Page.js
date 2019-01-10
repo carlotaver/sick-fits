df4d59acb5ed23fad0df419b53c9c8d274321890
Carlota Fernández <cavefe@gmail.com>
2019-01-10 08:57:05 +0100

    add in page and header media and more changes en frontend

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,17 +1,17 @@
 import React from "react";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 
 import Meta from "./Meta";
 import Header from "./Header";
 
 const defaultTheme = {
-  red: "#FFOOOO",
+  red: "#FF0000",
   black: "#393939",
   grey: "#3A3A3A",
-  lightgrey: "#E1E1E1",
+  lightGrey: "#E1E1E1",
   offWhite: "#EDEDED",
-  maxWidth: "1000px",
-  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09"
+  mediaMaxWidth: "1300px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
 };
 
 const StyledPage = styled.div`
