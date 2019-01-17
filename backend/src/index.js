35aa591775117a707f42551046ee1f0971216d7c
Carlota Fernández <carlotaverdejofernandezn@gmail.com>
2019-01-17 13:12:36 +0100

    changes in index in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config({path:'variables.env'});
-const createServer = require('createServer');
+const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
@@ -8,11 +8,12 @@ const server = createServer();
 // TODO Use express middlware to populate current user
 
 server.start(
-{
+  {
    cors: {
     credentials:true,
     origin: process.env.FRONTEND_URL,
-  },
-}, deets => {
+   },
+  }, 
+  deets => {
   console.log(`Server is now running on port http:/localhost:${deets.port}`);
 });
