cf13a2d79fd47764f3a80611b3546bb6bdeb0f2e
Carlota Fernández <carlotaverdejofernandezn@gmail.com>
2019-01-18 10:48:42 +0100

    changes in backend:src(createServer,resolvers(mutation,query),schema)and package.json

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,4 +1,4 @@
-const { GraphQlServer } = require("graphql-yoga");
+const { GraphQLServer } = require("graphql-yoga");
 const Mutation = require("./resolvers/Mutation");
 const Query = require("./resolvers/Query");
 const db = require("./db");
@@ -6,7 +6,7 @@ const db = require("./db");
 // Create the GraphQL Yoga Server
 
 function createServer() {
-  return new GraphQlServer({
+  return new GraphQLServer({
     typeDefs: "src/schema.graphql",
     resolvers: {
       Mutation,
