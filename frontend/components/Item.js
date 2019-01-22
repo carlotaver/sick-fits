9fdb30bdd67beaf056cc7b4c1cb338be4e9000ec
Carlota Fernández <carlotaverdejofernandezn@gmail.com>
2019-01-22 11:32:51 +0100

    create in pages:items and in components:Items ,Item IN frontend

---
+++ frontend/components/Item.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import Title from "./styles/Title";
+import ItemStyles from "./styles/ItemStyles";
+import PriceTag from "./styles/PriceTag";
+
+export default class Item extends Component {
+  static PropTypes = {
+    item: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { item } = this.props;
+    return (
+      <ItemStyles>
+        <Title>
+          <Link
+            href={{
+              pathname: "/item",
+              query: { id: item.id }
+            }}
+          >
+            <a> {item.title}</a>
+          </Link>
+        </Title>
+      </ItemStyles>
+    );
+  }
+}
